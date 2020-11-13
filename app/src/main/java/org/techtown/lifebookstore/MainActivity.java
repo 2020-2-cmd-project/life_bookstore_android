ea21f7758bf4a504db104571732f225a98b58d5b
Serin Yoon <bcode429@gmail.com>
2020-11-13 16:51:59 +0900

    first commit

---
+++ app/src/main/java/org/techtown/lifebookstore/MainActivity.java
@@ -0,0 +1,21 @@
+package org.techtown.lifebookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void movetoSearch(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+}
