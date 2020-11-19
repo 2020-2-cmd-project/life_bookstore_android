84a56a8e9b047aec82041f7dd1d2007fcb86c76c
Eunhee-JJ <eunhee0324@g.skku.edu>
2020-11-19 22:33:40 +0900

    [ADD] 상세보기 액티비티, 글 작성 액티비티 생성

---
+++ app/src/main/java/org/techtown/lifebookstore/WriteActivity.java
@@ -0,0 +1,14 @@
+package org.techtown.lifebookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
